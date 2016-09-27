53efbbbb34f6ea75a3a846f8961905149f090d7c
Justin <justwright@me.com>
2016-09-26 23:49:53 -0700

    Created gulp tasks for building and watching js and sass files. Also incorporated babel

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 gulp.task('scss', () =>
   gulp.src('./src/styles/**/*.scss')
@@ -17,14 +20,42 @@ gulp.task('scss', () =>
     .pipe(sourcemaps.write())
     .pipe(rename('milestones.css'))
     .pipe(gulp.dest('./src/styles'))
-    .pipe(gulp.dest('./dist/styles'))
+);
+
+gulp.task('js', () =>
+  gulp.src('./src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(rename('milestones.js'))
+    .pipe(gulp.dest('./src/js'))
 );
 
 gulp.task('build-html', () => {
-    gulp.src('./src/*.html')
+  gulp.src('./src/*.html')
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch', () =>
+gulp.task('build-css', ['scss'], () => {
+  gulp.src('./src/**/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-js', ['js'], () => {
+  gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('build-assets', () => {
+  gulp.src('./src/assets/**')
+    .pipe(gulp.dest('./dist/assets'));
+});
+
+gulp.task('build', ['build-css', 'build-js', 'build-html', 'build-assets']);
+
+gulp.task('watch', () => {
   gulp.watch('./src/styles/**/*.scss', ['scss'])
-);
+  gulp.watch('./src/**/*.js', ['js'])
+});
---
+++ src/js/partials/index.js
@@ -0,0 +1,7 @@
+const blahblah = 2;
+
+const array = [1,2,3,4];
+
+const theFunction = array.map((numb) => {
+    return numb * 4;
+});
