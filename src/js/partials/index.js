f6f50ac108e013cb24e588902e5eec3c60dc99e4
Justin Wright <justwright@me.com>
2017-06-04 16:01:14 -0700

    Re-arranged some html files; added button to scroll to contact section at the bottom of the page

--- src/js/partials/index.js
+++ src/js/partials/index.js
@@ -1,11 +1,12 @@
 const Flickity = require('flickity');
+const jump = require('jump.js');
 
-const sliderElement = document.querySelector('.carousel');
-const slider = new Flickity(sliderElement, {
+const sliderElement = document.querySelector('.carousel') ? document.querySelector('.carousel') : null;
+const slider = sliderElement ? new Flickity(sliderElement, {
     pageDots: false,
     wrapAround: true,
     autoPlay: 4000
-});
+}) : null;
 
 const initYear = () => {
     const yearElement = document.querySelector('.year');
@@ -15,11 +16,19 @@ const initYear = () => {
     yearElement.innerHTML = thisYear;
 };
 
+const contactButton = document.getElementById('contactScroll');
+
 // Init functions
 document.addEventListener('DOMContentLoaded', () => {
     initYear();
+
+    contactButton.addEventListener('click', () => {
+        jump(document.body.scrollHeight);
+    });
 });
 
 window.onload = () => {
-    slider.resize();
+    if (slider) {
+        slider.resize();
+    }
 };
