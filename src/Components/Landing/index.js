3cb10a83b099e773675ab88b558dbdb45b0077c7
Justin Wright <justwright@me.com>
2017-06-13 23:44:19 -0700

    Added styles and markup to the text on the landing page

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -5,11 +5,13 @@ class Landing extends Component {
     return (
       <div className="mainContent splash">
         {/*TODO slideshow component here*/}
-        <h2>The Path to Successful Childhood Education Starts Here</h2>
-        <p>Welcome to Milestones Academy Childcare Center, an environment of active exploration and discovery for children ages 18 months – 6 years. We are excited and proud to open our new location in the Orange County region, offering preschool
+        <section className="container splashBlockContainer">
+          <h2>The Path to Successful Childhood Education Starts Here</h2>
+          <p>Welcome to Milestones Academy Childcare Center, an environment of active exploration and discovery for children ages 18 months – 6 years. We are excited and proud to open our new location in the Orange County region, offering preschool
           and pre-kindergarten programs for families located in Lake Forest, Laguna Hills, Mission Viejo, Foothill Ranch, and the surrounding areas. Milestones is founded on the philosophy that children, their families, and society benefit from
           high quality, early childhood education programs. We believe that there is an indisputable link between a child’s early enrichment and success later in life. We believe that teachers are resources, problem posers, and partners in learning.
           Explorative materials, language building activities, hands-on-science, social studies, dramatic play, and artistic expression enrich a child’s life experience.</p>
+        </section>
       </div>
     );
   }
